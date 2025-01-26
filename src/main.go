b68ec0ad7bd4d5454f35fb10a4bf55448f73e484
daria <kapoor.darya@gmai.com>
2025-01-27 00:32:02 +0300

    Add tests

--- src/main.go
+++ src/main.go
@@ -8,4 +8,10 @@ import (
 func main() {
     result := utils.Add(3, 4)
     fmt.Println("The result of adding 3 and 4 is:", result)
+
+    result = utils.Sub(3, 4)
+    fmt.Println("The result of subtracting 3 and 4 is:", result)
+
+    result = utils.Mul(3, 4)
+    fmt.Println("The result of multiplying 3 and 4 is:", result)
 }
--- tests/main_test.go
+++ tests/main_test.go
@@ -12,3 +12,11 @@ func TestAdd(t *testing.T) {
 		t.Errorf("Add(3, 4) = %d; want %d", result, expected)
 	}
 }
+
+func TestSub(t *testing.T) {
+	result := utils.Sub(3, 4)
+	expected := -1
+	if result != expected {
+		t.Errorf("Sub(3, 4) = %d; want %d", result, expected)
+	}
+}
